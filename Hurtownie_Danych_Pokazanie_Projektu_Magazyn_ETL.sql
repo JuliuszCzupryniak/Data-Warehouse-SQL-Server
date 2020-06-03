bc579a559b559b460dbdbee3b9036e3a606a4a55
Juliusz Czupryniak <48527642+JuliuszCzupryniak@users.noreply.github.com>
2020-06-03 23:39:06 +0200

    Add files via upload

---
+++ Hurtownie_Danych_Pokazanie_Projektu_Magazyn_ETL.sql
@@ -0,0 +1,20 @@
+--SELECT
+select * from   nieruchomosc_inne;
+select * from   nieruchomosc_maz;
+select * from   nieruchomosc;
+
+select * from   sprzedaz_nieruchomosci_temp;
+select * from   sprzedaz_nieruchomosci_insert;
+select * from   sprzedaz_nieruchomosci_update;
+select * from   sprzedaz_nieruchomosci_errors;
+
+ --DELETE
+delete  nieruchomosc_inne;
+delete  nieruchomosc_maz;
+delete  nieruchomosc;
+
+delete  sprzedaz_nieruchomosci_temp;
+delete  sprzedaz_nieruchomosci_insert;
+delete  sprzedaz_nieruchomosci_update;
+delete  sprzedaz_nieruchomosci_errors;
+
---
+++ Hurtownie_Danych_Pokazanie_Projektu_Model_Wymiarowy.sql
@@ -0,0 +1,12 @@
+--TABELA FAKTÓW
+
+select * from sprzedaz_nieruchomosci;
+
+--DELETE
+
+delete kod_pocztowy;
+delete miasto;
+delete powiat;
+delete wojewodztwo;
+
+delete sprzedaz_nieruchomosci;
